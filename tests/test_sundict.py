bee9df956cbc5bfd38b27831c5d74b16f755f6ee
cjhead <chead@linuxmail.org>
2023-09-24 08:41:04 -0400

    test(test_sundict.py): calls Translator attribute instead of method

--- tests/test_sundict.py
+++ tests/test_sundict.py
@@ -3,4 +3,4 @@ from translator.translate import Translator
 
 def test_create_sun_dict():
     translator = Translator()
-    translator.create_dict()
+    assert isinstance(translator.sun_symbols, dict)
